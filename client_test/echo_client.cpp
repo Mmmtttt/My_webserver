7745ca5574ee4396cf0d053aa2dfa0287f32d7c8
Mmmtttt <mt1511318385@gmail.com>
2023-04-04 00:28:25 +0800

    test

--- client_test/echo_client.cpp
+++ client_test/echo_client.cpp
@@ -13,9 +13,11 @@ typedef std::pair<void*(*)(void*),void*> T;
 char s[40]="message from socket id : ";
 void*handle(void*arg);
 
+#define NUM 1000
 
 int main(){
-    int sock[1000];
+    int sock[NUM];
+    T*t[NUM];
 
     //sock=socket(PF_INET,SOCK_STREAM,0);
     sockaddr_in serv_addr;
@@ -23,20 +25,23 @@ int main(){
     memset(&serv_addr,0,sizeof(serv_addr));
 
     serv_addr.sin_family=AF_INET;
-    serv_addr.sin_port=htons(9190);
+    serv_addr.sin_port=htons(9191);
     serv_addr.sin_addr.s_addr=inet_addr("127.0.0.1");
 
 
     threadPoll<T> poll(NULL);
-    for(int i=0;i<1000;i++){
+    for(int i=0;i<NUM;i++){
         sock[i]=socket(PF_INET,SOCK_STREAM,0);
         if(connect(sock[i],(sockaddr*)&serv_addr,sizeof(serv_addr))==-1)std::cout<<"connect error"<<std::endl;
         std::pair<int,int> *arg=new std::pair<int,int>(sock[i],i);
         
-        T *t=new T(handle,(void*)arg);
-        poll.append(t);
+        t[i]=new T(handle,(void*)arg);
+        poll.append(t[i]);
     }
-
+    getchar();
+    for(int i=0;i<NUM;i++){
+        delete t[i];
+    }
     
 
     return 0;
